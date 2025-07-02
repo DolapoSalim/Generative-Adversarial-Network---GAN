f509a6c957f50c3d96e3f604b28e8022ec240d7f
Dolapo Salim Olatoye <96884730+DolapoSalim@users.noreply.github.com>
2025-07-02 16:10:40 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,44 @@
+# Operating System module for interacting with the operating system
+import os
+
+# Module for generating random numbers
+import random
+
+# Module for numerical operations
+import numpy as np
+
+# OpenCV library for image processing
+import cv2
+
+# Python Imaging Library for image processing
+from PIL import Image, ImageDraw, ImageFont
+
+# PyTorch library for deep learning
+import torch
+
+# Dataset class for creating custom datasets in PyTorch
+from torch.utils.data import Dataset
+
+# Module for image transformations
+import torchvision.transforms as transforms
+
+# Neural network module in PyTorch
+import torch.nn as nn
+
+# Optimization algorithms in PyTorch
+import torch.optim as optim
+
+# Function for padding sequences in PyTorch
+from torch.nn.utils.rnn import pad_sequence
+
+# Function for saving images in PyTorch
+from torchvision.utils import save_image
+
+# Module for plotting graphs and images
+import matplotlib.pyplot as plt
+
+# Module for displaying rich content in IPython environments
+from IPython.display import clear_output, display, HTML
+
+# Module for encoding and decoding binary data to text
+import base64
